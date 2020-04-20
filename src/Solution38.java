5b8e594698e4bc44330b4e1dc7c91419bed919f6
zhaoxukl1314 <zhaoxuk13kl>
2020-04-20 22:52:52 +0800

    init

---
+++ src/Solution38.java
@@ -0,0 +1,22 @@
+public class Solution38 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String countAndSay(int n) {
+        if (n == 1) {
+            return "1";
+        }
+
+        int position = 0;
+        int current = 0;
+
+        String s = countAndSay(n-1);
+
+        for (int i = 0; i < s.length(); i++) {
+
+        }
+        return "";
+    }
+}
